c136ba8d223a2f602ca1cadce4aa4a4c64ad4eab
RicardoLuz <contato@ricardoluz.com.br>
2022-07-06 10:41:09 -0300

    Mudança para MySQL

--- site_v01/settings.py
+++ site_v01/settings.py
@@ -88,13 +88,23 @@ WSGI_APPLICATION = 'site_v01.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'terapeutta',
+        'USER': 'teraadmin',
+        'PASSWORD': 'teRapeutta#8483',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
